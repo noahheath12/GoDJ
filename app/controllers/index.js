a22282e15b8f910ce3feed37df6dae6ed827ec2c
Noah Heath <noahheath12@yahoo.com>
2015-04-09 16:13:01 -0400

    Base Upload of Go DJ App

---
+++ app/controllers/index.js
@@ -0,0 +1,87 @@
+Alloy.Globals.tabGroup = $.tabGroup;
+// check if there is a user session saved already
+var aUser = Alloy.createModel('User');
+if (aUser.authenticated()) {
+	// rehydrate the user
+	aUser.showMe().then(function(_user) {
+		userLoggedIn(_user);
+	}, function(_error) {
+		alert("Application Error\n " + _response.error.message);
+		Ti.API.error(JSON.stringify(_response.error, null, 2));
+		// go ahead and do the login
+		userNotLoggedIn();
+	});
+} else {
+	userNotLoggedIn();
+}
+
+/**
+ * 
+ */
+function userLoggedIn(_user) {
+		
+	if ( !$.alreadyOpenedIndex ) {
+		// start the application
+		$.tabGroup.open();
+		
+		$.alreadyOpenedIndex = true;
+	}
+	
+	Alloy.Globals.CURRENT_USER = _user;
+	
+	// set button title with name to show we are logged in
+	$.logoutBtn.title = "Logout";
+
+
+	// added support for getting location from the user
+	// object since it seems like a helpful feature
+	_user.getCurrentLocation().then(function(_results) {
+		Ti.API.debug('_results ' + JSON.stringify(_results, null, 2));
+	}, function(_error) {
+		Ti.API.error('_error ' + JSON.stringify(_error));
+	});
+	
+}
+/**
+ *
+ */
+function userNotLoggedIn() {
+	// display login information
+	var ctrl = Alloy.createController('User', {
+		callback : function(_user) {
+			
+			userLoggedIn(_user);
+			
+			// close the old window
+			ctrl.getView().close();
+			ctrl = nil;
+		}
+	});
+
+	ctrl.getView().open();
+}
+
+/**
+ * 
+ */
+function doLogout() {
+	if (Alloy.Globals.CURRENT_USER) {
+		Alloy.Globals.CURRENT_USER.logout().then(function(_model){
+			Alloy.Globals.CURRENT_USER = null;
+			console.log("logged out!");
+			
+			// display login window
+			userNotLoggedIn();
+			
+		}, function(_error){
+			alert(_error.message);
+		});
+	}
+}
+
+$.index.addEventListener('androidback',function(e){
+    $.index.close();
+
+    // get activity and cleanup
+	$.index.activity.finish();
+});
